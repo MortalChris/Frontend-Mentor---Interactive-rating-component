0b2f54bfc839bed3adaf894a2e48f19c02a887a2
Christian Valeri <mortlaxris@outlook.com>
2022-05-23 16:55:58 -0500

    Added files for second page after clicking submit button

---
+++ Work/app.js
@@ -0,0 +1,13 @@
+const submit = document.getElementById("forms1")
+
+forms1.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const form = document.getElementById("forms")
+}
+
+//IDK why this isn't working
+$('.buttons').click(function(){
+    $(".buttons").removeClass("active");
+    $(this).addClass("active");
+  });
